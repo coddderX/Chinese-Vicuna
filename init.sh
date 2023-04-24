42930bccd35ec40f7ea51dd3a49adf0678a2e7c8
向辉 <xianghui@corp.netease.com>
2023-04-24 15:47:07 +0800

    update

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 cd /notebook
 apt-get install git-lfs
+rm -rf llama-7b-hf
+rm -rf Chinese-Vicuna
 git clone https://huggingface.co/decapoda-research/llama-7b-hf
 git clone https://github.com/xianghui123/Chinese-Vicuna
 cd Chinese-Vicuna
